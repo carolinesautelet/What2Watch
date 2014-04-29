4d68e86018e3374ee5881b79a1a2c00ca166d02f
Caroline Sautelet <caroline.sautelet.delorme@hotmail.com>
2014-04-29 16:47:19 +0200

    first version

---
+++ src/com/example/what2watch/Cinema_option.java
@@ -0,0 +1,32 @@
+package com.example.what2watch;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Cinema_option extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.cinema);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onBackPressed() {
+	    super.onBackPressed();
+	    overridePendingTransition(R.anim.slide_in2,R.anim.slide_out2);
+	}
+}
