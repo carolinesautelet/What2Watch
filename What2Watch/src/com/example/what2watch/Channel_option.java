9d3bc2fe8f30771fc1909f1cb8f022e313fb7bdc
Caroline Sautelet <caroline.sautelet.delorme@hotmail.com>
2014-04-29 17:16:47 +0200

    drop

---
+++ What2Watch/src/com/example/what2watch/Channel_option.java
@@ -0,0 +1,27 @@
+package com.example.what2watch;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Channel_option extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.channel);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
