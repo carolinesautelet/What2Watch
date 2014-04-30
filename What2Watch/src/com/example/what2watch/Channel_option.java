e57b2a8fa36f2a9cc201dc26b95f51d4f4160cf0
mokaddem <sami.mokaddem@student.uclouvain.be>
2014-04-30 14:12:19 +0200

    Modifiaction Sandrine Channel

--- What2Watch/src/com/example/what2watch/Channel_option.java
+++ What2Watch/src/com/example/what2watch/Channel_option.java
@@ -1,10 +1,13 @@
 package com.example.what2watch;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Channel_option extends Activity {
 
@@ -15,13 +18,57 @@ public class Channel_option extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
                                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.channel);
-	}
+		
+		Button playingToday=null;
+		Button channel=null;
+		
+        channel =(Button)findViewById(R.id.channel_button_channel);
+		playingToday = (Button) findViewById(R.id.channel_button_playingtoday);
+        
+        channel.setOnClickListener(listenerchannel);
+		playingToday.setOnClickListener(listenerplayingtoday);
+        
+		}
+	
+    	private OnClickListener listenerchannel = new OnClickListener() {
+    		@Override
+      		public void onClick(View v) {
+    			String queryString  = "SELECT DISTINCT rowid as _id, Name, ID FROM Channel ORDER BY Name ASC";
+    			String[] display = {"Name"};
+    			String[] whereArgs = null;
+    			send(queryString, whereArgs, display, "Channel");
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+    		}
+    	};
+    	
+        private OnClickListener listenerplayingtoday = new OnClickListener() {
+    		@Override
+      		public void onClick(View v) {
+    			String queryString  = "SELECT rowid as _id, Name, ID FROM Channel WHERE DATE(Time) = CURRENT_DATE";
+    			String[] display = {"Name"};
+    			String[] whereArgs = null;
+    			send(queryString, whereArgs, display, "Channel");
+    		}
+    	};
+    	
+    	public void send(String query, String[] args, String[] WhatToDisplay, String browse_by)
+    	{
+    		Intent intent = new Intent(Channel_option.this, List_of_request.class);
+    		
+    		Bundle bundle = new Bundle();
+    		bundle.putString("requete", query);
+    		bundle.putStringArray("arguments", args);
+    		bundle.putStringArray("display", WhatToDisplay);
+    		bundle.putString("search_by", browse_by);
+    		intent.putExtras(bundle);
+    		
+    		startActivity(intent);
+    		overridePendingTransition(R.anim.slide_in1,R.anim.slide_out1);
+    	}
+    	
+    	@Override
+    	public void onBackPressed() {
+    	    super.onBackPressed();
+    	    overridePendingTransition(R.anim.slide_in2,R.anim.slide_out2);
+    	}
 }
