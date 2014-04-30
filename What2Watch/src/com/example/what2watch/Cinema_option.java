0749e0f2ecbf28b4968f9d249ad99d76517d9bce
mokaddem <sami.mokaddem@student.uclouvain.be>
2014-04-30 14:27:52 +0200

    ajout modification Sandrine Cinema_option

--- What2Watch/src/com/example/what2watch/Cinema_option.java
+++ What2Watch/src/com/example/what2watch/Cinema_option.java
@@ -2,9 +2,12 @@ package com.example.what2watch;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Cinema_option extends Activity {
 
@@ -15,13 +18,65 @@ public class Cinema_option extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
                                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.cinema);
-	}
+		
+		Button comingsoon=null;
+		Button closest=null;
+		Button all=null;
+	
+		comingsoon =(Button)findViewById(R.id.cinema_button_coming_soon);
+		closest = (Button) findViewById(R.id.cinema_button_closest_cinema);
+		all = (Button) findViewById(R.id.cinema_button_all_cinemas);
+		
+		comingsoon.setOnClickListener(listenercomingsoon);
+		closest.setOnClickListener(listenerclosest);
+		all.setOnClickListener(listenerall);
+    
+		}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private OnClickListener listenercomingsoon = new OnClickListener() {
+		@Override
+  		public void onClick(View v) {
+			String queryString  = "SELECT rowid as _id, Name, ID FROM Cinema WHERE DATE(Time) >= CURRENT_DATE";
+			String[] display = {"Name"};
+			String[] whereArgs = null;
+			send(queryString, whereArgs, display, "Cinema");
+
+		}
+	};
+	
+    private OnClickListener listenerclosest = new OnClickListener() {
+		@Override
+  		public void onClick(View v) {
+			String queryString  = "SELECT rowid as _id, Name, ID FROM Cinema WHERE DATE(Time) >= CURRENT_DATE";
+			String[] display = {"Name"};
+			String[] whereArgs = null;
+			send(queryString, whereArgs, display, "Cinema");
+		}
+	};
+	
+    private OnClickListener listenerall = new OnClickListener() {
+		@Override
+  		public void onClick(View v) {
+			String queryString  = "SELECT rowid as _id, Name, ID FROM Cinema GROUP BY Name";
+			String[] display = {"Name"};
+			String[] whereArgs = null;
+			send(queryString, whereArgs, display, "Cinema");
+		}
+	};
+	
+	public void send(String query, String[] args, String[] WhatToDisplay, String browse_by)
+	{
+		Intent intent = new Intent(Cinema_option.this, List_of_request.class);
+		
+		Bundle bundle = new Bundle();
+		bundle.putString("requete", query);
+		bundle.putStringArray("arguments", args);
+		bundle.putStringArray("display", WhatToDisplay);
+		bundle.putString("search_by", browse_by);
+		intent.putExtras(bundle);
+		
+		startActivity(intent);
+		overridePendingTransition(R.anim.slide_in1,R.anim.slide_out1);
 	}
 
 	@Override
@@ -29,5 +84,4 @@ public class Cinema_option extends Activity {
 	    super.onBackPressed();
 	    overridePendingTransition(R.anim.slide_in2,R.anim.slide_out2);
 	}
-	
 }
